01467d7244c15df306ef1d85cc6154e53d082092
syedanwash-lab <syed.anwash@gmail.com>
2026-03-30 00:39:19 +0530

    Change Terraform backend S3 bucket and region
    Updated S3 bucket name and region for Terraform backend.

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,12 +1,14 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
-    region       = "us-east-1"
+    bucket       = "anvashdev-tf-bucket"
+    region       = "ap-south-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
     use_lockfile = true
   }
+
   required_version = ">=1.13.3"
+
   required_providers {
     aws = {
       version = ">= 6.23.0"
